03701bd6478be958bee7f3c254afff4f7d5b913e
kinfen <kinfen.in@gmail.com>
2020-03-03 11:20:22 +0800

    tmp

--- admin/server/api/session/signin.js
+++ admin/server/api/session/signin.js
@@ -4,7 +4,7 @@ var session = require('../../../../lib/session');
 function signin (req, res) {
 	var keystone = req.keystone;
 	if (!keystone.security.csrf.validate(req)) {
-		return res.apiError(403, 'invalid csrf');
+		return res.status(403).json( { error: 'invalid csrf'} );
 	}
 	if (!req.body.email || !req.body.password) {
 		return res.status(401).json({ error: 'email and password required' });
--- admin/server/app/createStaticRouter.js
+++ admin/server/app/createStaticRouter.js
@@ -15,26 +15,26 @@ module.exports = function createStaticRouter (keystone) {
 
 	// TODO: 处理react-select的样式问题
 	/* Prepare LESS options */
-	var elementalPath = path.join(path.dirname(require.resolve('elemental')), '..');
-	var reactSelectPath = path.join(path.dirname(require.resolve('react-select')), '..');
-	var keystoneTinymcePath = path.dirname(require.resolve('keystone-tinymce'));
-	var customStylesPath = keystone.getPath('adminui custom styles') || '';
+	// var elementalPath = path.join(path.dirname(require.resolve('elemental')), '..');
+	// var reactSelectPath = path.join(path.dirname(require.resolve('react-select')), '..');
+	// var keystoneTinymcePath = path.dirname(require.resolve('keystone-tinymce'));
+	// var customStylesPath = keystone.getPath('adminui custom styles') || '';
 
-	var lessOptions = {
-		render: {
-			javascriptEnabled: true,
-			modifyVars: {
-				elementalPath: JSON.stringify(elementalPath),
-				reactSelectPath: JSON.stringify(reactSelectPath),
-				keystoneTinymcePath: JSON.stringify(keystoneTinymcePath),
-				customStylesPath: JSON.stringify(customStylesPath),
-				adminPath: JSON.stringify(keystone.get('admin path')),
-			},
-		},
-	};
+	// var lessOptions = {
+	// 	render: {
+	// 		javascriptEnabled: true,
+	// 		modifyVars: {
+	// 			elementalPath: JSON.stringify(elementalPath),
+	// 			reactSelectPath: JSON.stringify(reactSelectPath),
+	// 			keystoneTinymcePath: JSON.stringify(keystoneTinymcePath),
+	// 			customStylesPath: JSON.stringify(customStylesPath),
+	// 			adminPath: JSON.stringify(keystone.get('admin path')),
+	// 		},
+	// 	},
+	// };
 	/* Configure router */
-	router.use('/styles', less(path.resolve(__dirname + '/../../public/styles'), lessOptions));
-	router.use('/styles/fonts', express.static(`${keystoneTinymcePath}/skin/fonts`));
+	// router.use('/styles', less(path.resolve(__dirname + '/../../public/styles'), lessOptions));
+	// router.use('/styles/fonts', express.static(`${keystoneTinymcePath}/skin/fonts`));
 	router.use(express.static(path.resolve(__dirname + '/../../public')));
 
 	return router;
